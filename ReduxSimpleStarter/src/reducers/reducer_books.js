e560387cf53cdae032f192a330f59fe010c89732
atafs <americoviagens@gmail.com>
2017-08-16 16:12:34 +0100

    42: Implementation of a container class

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts'},
+    { title: 'Python: The Best Parts'},
+    { title: 'Golang: The All Parts'},
+    { title: 'React and Redux: The Amazing Parts'}
+  ]
+}
