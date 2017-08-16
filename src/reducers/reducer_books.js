3d2229ce96aae6470787ffdc0519a2740b195160
atafs <americoviagens@gmail.com>
2017-08-16 16:12:34 +0100

    APP: List books and View details about it (section 4)

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,20 @@
+export default function() {
+  return [
+    {
+      title: 'Javascript: The Good Parts',
+      pages: 101
+    },
+    {
+      title: 'Python: The Best Parts',
+      pages: 23
+    },
+    {
+      title: 'Golang: The All Parts',
+      pages: 345
+    },
+    {
+      title: 'React and Redux: The Amazing Parts',
+      pages: 2
+    }
+  ]
+}
